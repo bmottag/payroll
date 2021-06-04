7ff5059b8923068a2a698fb6f51cce9bec661b07
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-06-04 13:21:15 -0500

    Nueva aplicacion

---
+++ application/logs/log-2021-06-03.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-06-03 13:36:52 --> 404 Page Not Found: ../modules/settings/controllers/Settings/manuals
+ERROR - 2021-06-03 13:49:07 --> La dirección para subir el archivo parece no ser válida.
+ERROR - 2021-06-03 13:50:22 --> La dirección para subir el archivo parece no ser válida.
+ERROR - 2021-06-03 13:50:56 --> La dirección para subir el archivo parece no ser válida.
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 4
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 4
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 46
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 46
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 47
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 47
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 60
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 98
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 98
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 99
+ERROR - 2021-06-03 16:00:17 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\external\views\procesos.php 99
---
+++ application/logs/log-2021-06-04.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-06-04 11:19:07 --> 404 Page Not Found: /index
+ERROR - 2021-06-04 11:19:15 --> 404 Page Not Found: /index
+ERROR - 2021-06-04 11:19:35 --> 404 Page Not Found: /index
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 9
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 9
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 10
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 10
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 15
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 15
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type bool C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 16
+ERROR - 2021-06-04 11:20:18 --> Severity: Notice --> Trying to access array offset on value of type null C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\form_documentos.php 16
+ERROR - 2021-06-04 11:27:45 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 15
+ERROR - 2021-06-04 11:27:45 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 17
+ERROR - 2021-06-04 11:37:51 --> Severity: Warning --> Use of undefined constant x - assumed 'x' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
+ERROR - 2021-06-04 11:37:51 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 15
+ERROR - 2021-06-04 11:37:51 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 17
+ERROR - 2021-06-04 11:37:52 --> Severity: Warning --> Use of undefined constant x - assumed 'x' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
+ERROR - 2021-06-04 11:37:52 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 15
+ERROR - 2021-06-04 11:37:52 --> Severity: Notice --> Undefined variable: estado C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 17
+ERROR - 2021-06-04 11:38:38 --> Severity: Warning --> Use of undefined constant x - assumed 'x' (this will throw an Error in a future version of PHP) C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
+ERROR - 2021-06-04 12:06:34 --> Severity: error --> Exception: Too few arguments to function Settings::documentos_procesos(), 1 passed in C:\xampp\htdocs\jbb_documentos\system\core\CodeIgniter.php on line 532 and exactly 2 expected C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
+ERROR - 2021-06-04 12:10:46 --> Severity: error --> Exception: Too few arguments to function Settings::documentos_procesos(), 1 passed in C:\xampp\htdocs\jbb_documentos\system\core\CodeIgniter.php on line 532 and exactly 2 expected C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
+ERROR - 2021-06-04 12:11:03 --> La dirección para subir el archivo parece no ser válida.
+ERROR - 2021-06-04 12:17:17 --> Severity: error --> Exception: syntax error, unexpected 'class' (T_CLASS) C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 97
+ERROR - 2021-06-04 12:17:26 --> Severity: error --> Exception: syntax error, unexpected 'class' (T_CLASS) C:\xampp\htdocs\jbb_documentos\application\modules\settings\views\documentos.php 97
+ERROR - 2021-06-04 12:18:04 --> Severity: error --> Exception: Too few arguments to function Settings::documentos_procesos(), 1 passed in C:\xampp\htdocs\jbb_documentos\system\core\CodeIgniter.php on line 532 and exactly 2 expected C:\xampp\htdocs\jbb_documentos\application\modules\settings\controllers\Settings.php 318
