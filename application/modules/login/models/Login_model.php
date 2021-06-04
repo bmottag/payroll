444660287450c411d8e7e243d114136d53105ba4
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-06-04 15:27:53 -0500

    Configuracion inicial

--- application/modules/login/models/Login_model.php
+++ application/modules/login/models/Login_model.php
@@ -21,7 +21,7 @@
 	    	$passwd = str_replace(array("<",">","[","]","*","^","-","'","="),"",$arrData["passwd"]); 
 			$passwd = md5($passwd);
 			
-	    	$sql = "SELECT * FROM usuarios WHERE log_user = '$login' and password = '$passwd'";
+	    	$sql = "SELECT * FROM user WHERE log_user = '$login' and password = '$passwd'";
 	    	$query = $this->db->query($sql);
 
 	    	if ($query->num_rows() > 0){	    		
@@ -35,12 +35,11 @@
 						$user["logUser"] = $row->log_user;
 	    				$user["movil"] = $row->movil;
 						$user["state"] = $row->state;
-						$user["role"] = $row->fk_id_user_role;
+						$user["rol"] = $row->perfil;
 						$user["photo"] = $row->photo;
 	    			//}	    			
 	    		}
 	    	}
-			
 			
 			//var_dump($user); die();
 	    	$this->db->close();	    	
@@ -56,93 +55,26 @@
 	    public function redireccionarUsuario()
 		{
 			$state = $this->session->userdata("state");
-			$userRole = $this->session->userdata("rol");
+			$userRol = $this->session->userdata("rol");
 			$dashboardURL = $this->session->userdata("dashboardURL");
 			
 	    	switch($state){
 	    		case 0: //NEW USER, must change the password
-	    				redirect("/usuarios","location",301);
+	    				redirect("/employee","location",301);
 	    				break;
 	    		case 1: //ACTIVE USER
-						redirect("settings/procesos","location",301);
+						redirect($dashboardURL,"location",301);
 	    				break;
 	    		case 2: //INACTIVE USER
 	    				$this->session->sess_destroy();
 	    				redirect("/login","location",301);
 	    				break;
-				case 66: //USUARIO QUE INGRESO CON LLAVE DE RECUPERACION, LO REDIRECCIONO AL CAMBIO DE CONTRASEÑA
-						redirect("/usuarios","location",301);
-						break;
 	    		default: //No sé como llegaron hasta acá, pero los devuelvo al Login.
 	    				$this->session->sess_destroy();
 	    				redirect("/login","location",301);
 	    				break;
 	    	}
 	    }
-		
-		/**
-		 * Guardo llave para recuperar contraseña
-		 * @since 25/11/2020
-		 */
-		public function saveKey($idUsuario, $email, $key) 
-		{				
-				$data = array(
-					'fk_id_user_ulc' => $idUsuario,
-					'email_user' => $email,
-					'llave' => $key
-				);	
-
-				$query = $this->db->insert('usuarios_llave_contraseña', $data);
-
-				if ($query) {
-					return true;
-				} else {
-					return false;
-				}
-		}
-		
-	    /**
-	     * Cargo los datos del usuario que viene con LLAVE
-	     * @author BMOTTAG
-	     * @since  25/11/2020
-	     */
-	    public function validateLoginKey($arrData)
-		{
-	    	$user = array();
-	    	$user["valid"] = false;
-			
-			$this->db->select();
-			$this->db->join('usuarios U', 'U.id_user = Q.fk_id_user_ulc', 'INNER');
-			$this->db->where('llave', $arrData["key"]);
-			$this->db->where('U.state', 1);			
-			$query = $this->db->get('usuarios_llave_contraseña Q');
-			
-	    	if ($query->num_rows() > 0){	    		
-	    		foreach($query->result() as $row){
-	    				$user["valid"] = true;
-	    				$user["id"] = $row->id_user;
-	    				$user["firstname"] = $row->first_name;
-	    				$user["lastname"] = $row->last_name;
-						$user["logUser"] = $row->log_user;
-						$user["email"] = $row->email;
-	    				$user["movil"] = $row->movil;
-						$user["state"] = $row->state;
-						$user["role"] = $row->fk_id_user_role;
-	    		}
-	    	}
-			
-	    	$this->db->close();	    	
-	    	return $user;
-	    }	
-	    
-	
-		
-		
-		
-		
-		
-		
-		
 		
 	    
 	}
