8923bfd6a3982530cdeb230806bcd3828eb44f0d
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-06-17 09:48:31 -0500

    Ajuste del titulo

--- application/views/layout.php
+++ application/views/layout.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="MottaClick">
 	<meta name="baseurl" content="<?php echo base_url()?>" />
-	<title>PayRoll</title>
+	<title>PAYROLLclick.</title>
 
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
